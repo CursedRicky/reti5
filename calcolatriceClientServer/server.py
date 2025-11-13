e4fa26fbe16d91525947b663cf1e3eeedbddc5d8
Riccardo Marzi <78096957+CursedRicky@users.noreply.github.com>
2025-11-13 17:54:00 +0100

    Create server.py

---
+++ calcolatriceClientServer/server.py
@@ -0,0 +1,39 @@
+import socket
+
+port = 17710
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('0.0.0.0', port))
+
+server.listen()
+print("Server in ascolto sulla porta: " + str(port))
+
+def handle(client):
+    while True:
+        msg = client.recv(1024).decode()
+        if len(str(msg)) != 0:
+            if msg == "|quit|":
+                break
+            else:
+                tot = eval(msg.replace("x", "*"))
+                client.send(("Risultato: " + str(tot)).encode("utf-8"))
+
+    # Connessione terminata, torna ad ascoltare
+    print("Client disconnesso")
+    ascolta()
+
+def ascolta():
+    print("...aspettando client...")
+    connectedClient = []
+    while True:
+        client, indirizzo = server.accept()
+        print(f"Connessione da {str(indirizzo)}")
+
+        connectedClient = client
+        break
+    handle(connectedClient)
+
+ascolta()
+
+
+
